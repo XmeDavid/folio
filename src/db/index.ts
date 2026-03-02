469d7a0be5412502869800fe9dd97128e286d7bf
David Batista <xmedavid@users.noreply.github.com>
2026-03-02 21:00:27 +0100

    debug: env test

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,5 +4,7 @@ import * as schema from "./schema";
 
 const connectionString = process.env.DATABASE_URL!;
 
+console.log(`[db] DATABASE_URL host: ${connectionString ? new URL(connectionString).host : "NOT SET"}`);
+
 const client = postgres(connectionString);
 export const db = drizzle(client, { schema });
